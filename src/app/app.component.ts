1a9d9a83cdbb81a05ebad2a1c58ba5fe1ad030ed
jfabiant <john.timoteo@tecsup.edu.pe>
2019-11-19 08:41:24 -0800

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,114 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductosService } from './productos.service';
+import { HttpClient } from '@angular/common/http';
+import swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  uploadedFiles: Array<File>;
+
+  lista = null;
+  prod = {
+    codigo: null,
+    descripcion: null,
+    precio: null,
+    imagen: null
+  };
+  constructor(private productosServicio: ProductosService) { }
+
+  // tslint:disable-next-line:use-lifecycle-interface
+  ngOnInit() {
+    this.recuperarTodos();
+  }
+
+  fileChange(element) {
+    this.uploadedFiles = element.target.files;
+  }
+
+  recuperarTodos() {
+    this.productosServicio.listar().subscribe(result => {
+      this.lista = result;
+    });
+  }
+
+  nuevo() {
+    if (this.prod.descripcion == null || this.prod.precio == null) {
+      swal.fire(
+        'COMPLETE',
+        'TODOS LOS CAMPOS SON REQUERIDOS',
+        'error'
+      );
+      return;
+    }
+    const formData = new FormData();
+
+    // tslint:disable-next-line:prefer-for-of
+    formData.append('descripcion', this.prod.descripcion);
+    formData.append('precio', this.prod.precio);
+    formData.append('imagen', this.uploadedFiles[0], this.uploadedFiles[0].name);
+
+    this.productosServicio.nuevo(formData).subscribe(result => {
+      swal.fire({
+        toast: true,
+        icon: 'success',
+        title: 'Registro satisfactorio',
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        onOpen: (toast) => {
+          toast.addEventListener('mouseenter', swal.stopTimer)
+          toast.addEventListener('mouseleave', swal.resumeTimer)
+        }
+      });
+      this.limpiar();
+      this.recuperarTodos();
+    });
+  }
+
+  eliminar(codigo) {
+    this.productosServicio.eliminar(codigo).subscribe(result => {
+      this.recuperarTodos();
+    });
+    this.recuperarTodos();
+  }
+
+  actualizar() {
+    if (this.prod.descripcion == null || this.prod.precio == null) {
+      return;
+    }
+    const formData = new FormData();
+    formData.append('codigo', this.prod.codigo);
+    formData.append('imagen', this.uploadedFiles[0], this.uploadedFiles[0].name);
+    formData.append('descripcion', this.prod.descripcion);
+    formData.append('precio', this.prod.precio);
+    this.productosServicio.actualizar(formData).subscribe(result => {
+      this.limpiar();
+      this.recuperarTodos();
+    });
+  }
+
+  mostrar(codigo) {
+    this.productosServicio.mostrar(codigo).subscribe(result => {
+      this.prod = result[0];
+    });
+  }
+
+  hayRegistros() {
+    return true;
+  }
+
+  limpiar() {
+    this.prod = {
+      codigo: null,
+      descripcion: null,
+      precio: null,
+      imagen: null
+    };
+  }
+}
